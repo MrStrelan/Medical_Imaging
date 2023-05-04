36d4f54b506a650e988bb8d8f98dfa6017e8104f
Mark <mr.assejev@gmail.com>
2023-05-04 18:05:52 +0200

    symmetrytojson added

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import numpy as np
 import Mole
 import numpy as np
+import os
+import json
 
 def main():
-    mole = Mole.Mole('PAT_91_140_664')
 
-  #  mole.show_per()
-    
 
-    print(f"Perimeter Sum: {np.sum(mole.perim)}")
-    
-   # mole.symmetric()
-    print(mole.symmetry_detection())
+    return
+
+def symmetry_to_json():
+    symValues = {}
+    for pic in os.listdir(".\\Medical_Imaging\\Images"):
+        mole = Mole.Mole(pic[:-4])
+
+    #  mole.show_per()
+        
+
+        print(f"Perimeter Sum: {np.sum(mole.perim)}")
+        
+    # mole.symmetric()
+        values = mole.symmetry_detection()
+        symValues.update({pic: values})
    # mole.show_seg_mask()
+        json_object = json.dumps(symValues, indent=4)
+        with open("symValues.json", "w") as outfile:
+            outfile.write(json_object)
+        print(pic, "done")
+    
+    return  print("Done!")
 
-    print("Done!")
+
+
 
 if __name__ == "__main__":
     main()
