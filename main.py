19e93cf08c500b62cd0ddd5843cc3f0346a3fcfa
MrStrelan <90510167+MrStrelan@users.noreply.github.com>
2023-05-29 16:12:32 +0200

    borders done

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import Mole
 def main():
 
     mole = Mole.Mole("PAT_10_18_830")
-    #a,b,v,g,d,e,j = mole.find_colors
-    #print("count", a, " a ", b, " b ", v, " v ", g, " d ", d, " e ", e, " j ", j)
-    mole.print_all()
+    #mole.print_all()
     return
 
 main()
